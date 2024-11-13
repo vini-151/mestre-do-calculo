2a57b31494834a7186cc6b525ce87fe1f5cecd4c
YasminRez <yasminrezende99@gmail.com>
2024-11-13 10:11:46 -0300

    start front voltei

--- src/View/Front.java
+++ src/View/Front.java
@@ -1,19 +1,6 @@
 package View;
 
-import javax.swing.*;
-
-public class Front extends JFrame {
-
-
-    public Front(){
-        super("Mestre do cálculo");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-
-
-    }
-
-
-
+public class Front {
+    String name;
+    String color;
 }
